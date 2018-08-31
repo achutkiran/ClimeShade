686a38c6c2d4a3231a254b3f59eb1954cce68b10
Achut Cherukuri <Achut.Cherukuri@cybersoftech.com>
2018-08-31 11:26:17 -0500

    Added comments

--- server/schema.js
+++ server/schema.js
@@ -1,27 +1,38 @@
 const {gql} = require('apollo-server-express');
  const schema = gql`
 	type Query {
+		"gives weather information for given zipcode"
 		weather(zipcode: Int!): Climate
+
+		" gives user details"
 		user(id: ID
 			 firstName: String
 			 lastName:String): [User] 
 	}
 
 	type Mutation{
+		"creation of new user"
 		createUser( firstName: String,
 					lastName: String,
 					zipcode: Int,
 					userName: String!,
-					password: String!): String
+					password: String!): String 
+
+		"deletion of user account"
 		removeUser(userId:ID!):String
+
+		"updates user information"
 		updateUser(userId: ID!,
 					firstName: String,
 					lastName: String,
 					zipcode:Int
 					password:String): String
+	
+		"login to user account"
 		login(userName:String!,
 			  password:String!):String
-
+			
+		"Update weather information in database"
 		updateWeather(zipcode:Int!): String
 	}
 
@@ -29,6 +40,7 @@ const {gql} = require('apollo-server-express');
 		userCreated: User
 	}
 	
+	"weather details"
 	type Climate {
 		zipcode: Int!
 		temperature: String!
@@ -39,6 +51,7 @@ const {gql} = require('apollo-server-express');
 		weatherCondition: String
 	}
 
+	"user details"
 	type User{
 		firstName: String
 		lastName: String
